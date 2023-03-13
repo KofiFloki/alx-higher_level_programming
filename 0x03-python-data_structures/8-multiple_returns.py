0417453777b9aa55bb4dd92ca513591ee679ccc7
KofiFloki <Jeremiah.martey@yahoo.com>
2023-03-13 12:18:18 -0700

    More returns!

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    lenght = len(sentence)
+    if length > 0:
+       use  = sentence[0]
+    else:
+        use = None
+    return (length, first)
