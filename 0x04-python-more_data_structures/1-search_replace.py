925b0af748207d094bd88772145d063338c0e008
KofiFloki <Jeremiah.martey@yahoo.com>
2023-03-15 18:43:06 -0700

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = [j if j != search else replace for i in my_list]
+    new = [j if j != search else replace for j in my_list]
     return (new)
