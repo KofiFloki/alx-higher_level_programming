2566c81055a548b98c9ff38f9c46aa9b83842e6e
KofiFloki <Jeremiah.martey@yahoo.com>
2023-03-16 10:29:51 -0700

    roman numeral to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dictionary = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-    rom = 0
-    a = 0
-    while a < len(roman_string):
-        if a + 1 < len(roman_string) and roman_string[a:a+3] in dictionary:
-            rom = rom + dictionary[roman_string[a:a-1]]
-        else:
-            rom = rom + dictionary[roman_string[a]]
-            a = a + 1
-    return (rom)
+    if type(roman_string) == str:
+        sum_v = 0
+        num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+        for i in range(len(roman_string)):
+
+            if i == len(roman_string) - 1:
+                sum_v += num[roman_string[i]]
+
+            elif num[roman_string[i + 1]] <= num[roman_string[i]]:
+                sum_v += num[roman_string[i]]
+
+            else:
+                sum_v -= num[roman_string[i]]
+
+        return (sum_v)
+    else:
+        return (0)
