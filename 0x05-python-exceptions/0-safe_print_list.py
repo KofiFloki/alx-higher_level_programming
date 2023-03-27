18dc4767b0ceaba865283081428e10537d40a24e
KofiFloki <Jeremiah.martey@yahoo.com>
2023-03-27 16:18:23 -0700

    my second commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    b = 0
-    try:
-        for c in range(x):
-            print(my_list[c], end=' ')
-            b += 1
-    except IndexError:
-        pass
-    finally:
-        print(('\n'), end=' ')
-        return b
+    n = 0
+    for m in range(x):
+        try:
+            print(my_list[m], end="")
+            n += 1
+        except IndexError:
+            break
+    print()
+    return n
