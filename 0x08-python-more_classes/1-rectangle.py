8274b2b00884deffad40ce7487f711319fadf08f
KofiFloki <Jeremiah.martey@yahoo.com>
2023-04-04 05:47:39 -0700

    My project commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,13 +7,13 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize the rectangle with a given width and height"""
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """Get the width of the rectangle"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -22,12 +22,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
         """Get the height of the rectangle"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -36,11 +36,11 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
 
     def area(self):
         """Calculate the area of the rectangle"""
-        return self._width * self._height
+        return self.__width * self.__height
 
     def perimeter(self):
         """Calculate the perimeter of the rectangle"""
