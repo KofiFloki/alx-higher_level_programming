a48d35c8e7f6dc2cfbf987cb98dfebc577d2360b
KofiFloki <Jeremiah.martey@yahoo.com>
2023-04-06 23:42:32 -0700

    My project commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,11 +7,13 @@ This function take in two strings and returns the joined strings
 
 
 def say_my_name(first_name, last_name=""):
+
     """
     Returns a combination of first_name and last_name
     """
+
     if type(first_name) not in [str]:
         raise TypeError("first_name must be a string")
     if type(last_name) not in [str]:
         raise TypeError("last_name must be a string")
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name)
