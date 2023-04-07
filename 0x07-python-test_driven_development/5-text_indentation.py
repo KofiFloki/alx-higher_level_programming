ac1b7a247e92d9e7b2e86c22a13deff47d872d95
KofiFloki <Jeremiah.martey@yahoo.com>
2023-04-06 23:59:48 -0700

    My project commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 """
-This 5-text indentation module defines one function, text_indentation().
-The function takes one parameter as a string of text and
-prints the text with 2 lines after each "." or "?"
+Module composed by a function that prints 2 new lines after ".?:" characters
 """
 
 
+def text_indentation(text):
+    """ Function that prints 2 new lines after ".?:" characters
+    Args:
+        text: input string
+    Returns:
+        No return
+    Raises:
+        TypeError: If text is not a string
+    """
 
-def print_square(size):
-    """prints a square using the character '#'"""
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    if type(size) is float and size < 0:
-        raise TypeError("size must be an integer")
-    a = 0
-    b = 0
-    while a < size:
-        while b < size:
-            print("#", end="")
-            b += 1
-        b = 0
-        a += 1
-        print()
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    s = text[:]
+
+    for d in ".?:":
+        list_text = s.split(d)
+        s = ""
+        for i in list_text:
+            i = i.strip(" ")
+            s = i + d if s is "" else s + "\n\n" + i + d
+
+    print(s[:-3], end="")
