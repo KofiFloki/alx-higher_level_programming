9b020af51e00ec631367583e554454f24b11bb71
KofiFloki <Jeremiah.martey@yahoo.com>
2023-04-06 23:50:37 -0700

    My project commit

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,18 +7,12 @@ This function takes one parameter used as the size of a square to print using #
 
 
 def print_square(size):
-    if type(size) is not int:
+    """ Function that prints a square with the character #"""
+
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if type(size) is float and size < 0:
-        raise TypeError("size must be an integer")
-    x = 0
-    y = 0
-    while x < size:
-        while y < size:
-            print("#", end="")
-            y += 1
-        y = 0
-        x += 1
-        print()
+
+    for i in range(size):
+        print("#" * size)
