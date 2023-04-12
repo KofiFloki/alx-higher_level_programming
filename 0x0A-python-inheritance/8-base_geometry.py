2f3c690fb2ce5b63425752a7faf39ca9612b94a5
KofiFloki <Jeremiah.martey@yahoo.com>
2023-04-11 17:53:11 -0700

    Rectangle

---
+++ 0x0A-python-inheritance/8-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+The `8-rectangle' module has just two classes,
+BaseGeometry() and Rectangle which is a sub
+class of BaseGeometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+imports the super class
+"""
+
+
+class Rectangle(BaseGeometry):
+    """defines a class Rectangle which is a subclass of BaseGeometry"""
+    def __init__(self, width, height):
+        """validates and initializes width and height"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
