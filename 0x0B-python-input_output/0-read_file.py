052fc4a8c35195b47f0688752578495ff4971282
KofiFloki <Jeremiah.martey@yahoo.com>
2023-04-12 22:35:40 -0700

    My project commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Thiscontains the read_file function"
+This contains the read_file function"
+"""
 
 
-def read_file(filename=""):i
-    """reads the test file UTF8  and prints it to stdout"""
-
-    with open(filename, "r", encoding="utf-8") as myFile:
-        print(myFile.read(), end="")
+def read_file(filename=""):
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+this is the 3-to_json_string module
+this module provides just one function
+to_json_string()
+"""
+
+
+import json
+"""needed to return the json representation of an object"""
+
+
+def to_json_string(my_obj):
+    """returns a json representation of an object"""
+    a = json.dumps(my_obj)
+    return a
+
